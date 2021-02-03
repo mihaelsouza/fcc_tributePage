16f58bfa17cc97a7d1b8505ceb0eba13aa5ab5a5
Mihael Machado de Souza <mihaelsouza@gmail.com>
2021-02-03 12:01:30 +0200

    added some responsive behavior using @media rules

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ const cardData = {
       src: "media/card1A.jpg",
       alt: "Drawing of people using face masks adorned with various country flags.",
       cc0: '<span class="cc0">Photo by <a target="_blank" href="https://unsplash.com/@ashkfor121?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Ashkan Forouzani</a> on <a target="_blank" href="https://unsplash.com/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span><br><br>',
-      text: "Body language has always been a huge part of social interactions, and facial expressions even more so. You could somewhat tell when someone was happy, angry or amused, and if necessary course correct. People say that the eyes are the window for the soul. If that is the case, the face is the whole wall and you can see even the paint chips sometimes."
+      text: "<center><strong>Face Masks</strong></center><br>Body language has always been a huge part of social interactions, and facial expressions even more so. You could somewhat tell when someone was happy, angry or amused, and if necessary course correct. People say that the eyes are the window for the soul. If that is the case, the face is the whole wall and you can see even the paint chips sometimes."
     },
     B: {
       src: "media/card1B.jpg",
       alt: "People wearing face masks adorned with the flags of different nations.",
       cc0: '<span class="cc0">Made available by the United Nations</span>',
-      text: "And suddenly that social input is not there anymore. Face masks become as common in the wardrobe as shirts and socks. For some, they are a nuisance. For others, a fashion statement. Either way, they became a necessity to fight a sprawling pandemic. On the upside, people probably stopped forgetting their dental hygiene in the mornings..."
+      text: "<center><strong>Face Masks</strong></center><br>And suddenly that social input is not there anymore. Face masks become as common in the wardrobe as shirts and socks. For some, they are a nuisance. For others, a fashion statement. Either way, they became a necessity to fight a sprawling pandemic. On the upside, people probably stopped forgetting their dental hygiene in the mornings..."
     },
     state: "A"
   },
@@ -19,13 +19,13 @@ const cardData = {
       src: "media/card2A.jpg",
       alt: "Company employees discussing strategy in a meeting room.",
       cc0: '<span class="cc0">Photo by <a target="_blank" href="https://unsplash.com/@youxventures?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">You X Ventures</a> on <a target="_blank" href="https://unsplash.com/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span><br><br>',
-      text: "Typical office day. Endless meetings and noise around the cubicles while you are trying to hammer away that last deliverable before the end of day deadline. You have already heard about the latest office gossip through the grapevine and Carol told you about that amazing weekend she is planning around the watercooler, while overhearing Bob talk about last night's game. At this point, all you can think about is getting home..."
+      text: "<center><strong>Office Environments</strong></center><br>Typical office day. Endless meetings and noise around the cubicles while you are trying to hammer away that last deliverable before the end of day deadline. You have already heard about the latest office gossip through the grapevine and Carol told you about that amazing weekend she is planning around the watercooler, while overhearing Bob talk about last night's game. At this point, all you can think about is getting home..."
     },
     B: {
       src: "media/card2B.jpg",
       alt: "Computer over a table next to a rustic mug. In the computer screen, a zoom meeting with more than a dozen participants.",
       cc0: '<span>Photo by <a href="https://unsplash.com/@cwmonty?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Chris Montgomery</a> on <a href="https://unsplash.com/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span><br><br>',
-      text: "And now, you're always home. ALWAYS... There is no longer that sense of 'getting home from work' as that is essentially just the other room. Or even worse, the bedroom is suddenly the office. Work and life boundaries get blurried. For some, this is heaven. For others, a particularly nasty nightmare. For almost everybody, a necessary adaptation."
+      text: "<center><strong>Office Environments</strong></center><br>And now, you're always home. ALWAYS... There is no longer that sense of 'getting home from work' as that is essentially just the other room. Or even worse, the bedroom is suddenly the office. Work and life boundaries get blurried. For some, this is heaven. For others, a particularly nasty nightmare. For almost everybody, a necessary adaptation."
     },
     state: "A"
   },
@@ -34,13 +34,13 @@ const cardData = {
       src: "media/card3A.jpg",
       alt: "Bustling city street at night with shops and stalls.",
       cc0: '<span class="cc0">Photo by <a target="_blank" href="https://unsplash.com/@keepsakes_and_daydreams?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Steven Skerritt</a> on <a target="_blank" href="https://unsplash.com/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span><br><br>',
-      text: "Friday night! Time to get fancy, put on your best clothes and join your friends for a night in the town. All you're looking for is cold beer or maybe a smooth drink with some good music. Oh, and definitely that huge $2 hotdog dripping in sauce that you can get by the gas station at the end of the night. Because, you know, Mike is such a nice guy!"
+      text: "<center><strong>Night Life</strong></center><br>Friday night! Time to get fancy, put on your best clothes and join your friends for a night in the town. All you're looking for is cold beer or maybe a smooth drink with some good music. Oh, and definitely that huge $2 hotdog dripping in sauce that you can get by the gas station at the end of the night. Because, you know, Mike is such a nice guy!"
     },
     B: {
       src: "media/card3B.jpg",
       alt: "Picture of an almost empty restaurant, with just a few patrons inside.",
       cc0: '<span>Photo by <a href="https://unsplash.com/@andyadcon?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">De an Sun</a> on <a href="https://unsplash.com/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span><br><br>',
-      text: "Restaurants and bars are outright closed, or closing early. Half of all seats are unavailable, and not because there is people there. Social distance guidelines and strict health measures make a huge impact on the hospitality business and take-away becomes the new normal. Friday night events boil down to food orders and zoom hangouts, trying to simulate a glimpse of the old normal."
+      text: "<center><strong>Night Life</strong></center><br>Restaurants and bars are outright closed, or closing early. Half of all seats are unavailable, and not because there is people there. Social distance guidelines and strict health measures make a huge impact on the hospitality business and take-away becomes the new normal. Friday night events boil down to food orders and zoom hangouts, trying to simulate a glimpse of the old normal."
     },
     state: "A"
   },
@@ -49,13 +49,13 @@ const cardData = {
       src: "media/card4A.jpg",
       alt: "New York City Central Station with a lot of people walking around.",
       cc0: '<span class="cc0">Photo by <a target="_blank" href="https://unsplash.com/@jackofallstreets?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Jack Finnigan</a> on <a target="_blank" href="https://unsplash.com/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span><br><br>',
-      text: "You've been planning this the whole year, after finally getting that vacation request approved. Time to enjoy some Mai Tais by a beach somewhere, or go skiing in the mountains. Of course, you have to get there first. And so you muster all your inner peace to brave the train station/airport crowds..."
+      text: "<center><strong>Travelling</strong></center><br>You've been planning this the whole year, after finally getting that vacation request approved. Time to enjoy some Mai Tais by a beach somewhere, or go skiing in the mountains. Of course, you have to get there first. And so you muster all your inner peace to brave the train station/airport crowds..."
     },
     B: {
       src: "media/card4B.jpg",
       alt: "Empty Central Station in New York in the middle of the afternoon.",
       cc0: '<span>Photo by <a href="https://unsplash.com/@apollo_y?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">A P O L L O</a> on <a href="https://unsplash.com/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span><br><br>',
-      text: "And now the crowds are not there. Airports and train stations are empty, as travel restrictions and closed borders halt international and national travels. Vacations are essentially just a pass to not turn on ZOOM for a week. Small victories, I suppose?"
+      text: "<center><strong>Travelling</strong></center><br>And now the crowds are not there. Airports and train stations are empty, as travel restrictions and closed borders halt international and national travels. Vacations are essentially just a pass to not turn on ZOOM for a week. Small victories, I suppose?"
     },
     state: "A"
   },
@@ -64,13 +64,13 @@ const cardData = {
       src: "media/card5A.jpg",
       alt: "Family and friends gathered together at the beach watching the sunset.",
       cc0: '<span class="cc0">Photo by <a target="_blank" href="https://unsplash.com/@jtylernix?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Tyler Nix</a> on <a target="_blank" href="https://unsplash.com/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span><br><br>',
-      text: "It's finally Christmas! Or the New Year?! ...Maybe Thanksgiving? Okay, definitely Earth Day! No matter, you are just looking forward to eating nana May's cobbler or aunt Mary's cherry pie. Just ready to enjoy some peaceful family time. (Okay, not always peaceful...)"
+      text: "<center><strong>The Holidays</strong></center><br>It's finally Christmas! Or the New Year?! ...Maybe Thanksgiving? Okay, definitely Earth Day! No matter, you are just looking forward to eating nana May's cobbler or aunt Mary's cherry pie. Just ready to enjoy some peaceful family time. (Okay, not always peaceful...)"
     },
     B: {
       src: "media/card5B.jpg",
       alt: "Parents and child decorating a Christmas tree.",
       cc0: '<span>Photo by <a href="https://unsplash.com/@jonathanborba?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Jonathan Borba</a> on <a href="https://unsplash.com/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span><br><br>',
-      text: "Nowadays families are discouraged to gather together if they can help it, specially to avoid putting elderly members in risk. Family units cope as they can, celebrating in their own households and connecting as best as possible. As a small silver lining, you still get to have those amazingly funny family stories, as you try to teach those tecnophobes how to install Skype or Zoom."
+      text: "<center><strong>The Holidays</strong></center><br>Nowadays families are discouraged to gather together if they can help it, specially to avoid putting elderly members in risk. Family units cope as they can, celebrating in their own households and connecting as best as possible. As a small silver lining, you still get to have those amazingly funny family stories, as you try to teach those tecnophobes how to install Skype or Zoom."
     },
     state: "A"
   }
@@ -97,6 +97,7 @@ var fillContentInDiv = function (dataIn, divName, state) {
     // Enables onclick functionality to change the image and text within each card
     document.getElementById(divName).innerHTML = ""; // Clear current content
     fillContentInDiv(cardData, divName, false); // Fills with content for alternate state
+    document.getElementById(`card${divName.slice(-1)}`).focus(); // Give focus to the newly added image
   }
   figure.appendChild(image);
 
@@ -109,7 +110,7 @@ var fillContentInDiv = function (dataIn, divName, state) {
   // Add the textual content to the paragraph
   if (!state) {paragraph.classList.add('anim-slide');}
   paragraph.classList.add('tribute-text');
-  paragraph.innerText = data['text'];
+  paragraph.innerHTML = data['text'];
 
   // Add these elements to the correct div, based on divName
   document.getElementById(divName).appendChild(figure);
